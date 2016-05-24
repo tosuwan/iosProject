63b0ea612a12254c46a1a60f36899bb0bc153777
Jongsuk Sun <tosuwan7@gmail.com>
2016-05-24 11:03:28 +0900

    Initial Commit

---
+++ objC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objC
+//
+//  Created by MF839-012 on 2016. 5. 24..
+//  Copyright © 2016년 MF839-012. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
