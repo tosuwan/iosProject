64497d0af5334b662d08e957090cac793ae68cfd
Jongsuk Sun <tosuwan7@gmail.com>
2016-05-24 15:06:09 +0900

    3 장 완료

--- objC/main.m
+++ objC/main.m
@@ -8,10 +8,69 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Fraction : NSObject
+    
+-(void) print;
+-(void) setNumerator:(int) n;
+-(void) setDenominator:(int) d;
+-(int) numerator;
+-(int) denominator;
+
+
+@end
+
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+
+-(void) print {
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator:(int)n {
+    numerator = n;
+}
+
+-(void) setDenominator:(int)d {
+    denominator = d;
+}
+
+-(int) numerator {
+    return numerator;
+}
+
+-(int) denominator {
+    return denominator;
+}
+
+@end
+
+
 int main(int argc, const char * argv[]) {
+    
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Programming is fun!");
+        //NSLog(@"Programming in Objective-C is even more fun!");
+        
+        //int sum;
+        
+        //sum = 50 + 25;
+        //NSLog(@"The sum of 50 and 25 is %i", sum);
+        
+        Fraction *myFraction;
+        
+        myFraction = [Fraction alloc];
+        myFraction = [myFraction init];
+        
+        [myFraction setNumerator:1];
+        [myFraction setDenominator:3];
+        
+        NSLog(@"The value of myFraction is : %i/%i", [myFraction numerator], [myFraction denominator]);
+        //[myFraction print];
+        
     }
     return 0;
 }
